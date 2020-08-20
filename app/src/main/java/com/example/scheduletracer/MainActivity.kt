c4596af10be964c1ce8b40fb71066bf7e2ab7bca
Erick <incspnsoft@gmail.com>
2020-08-20 15:47:03 -0700

    first commit

---
+++ app/src/main/java/com/example/scheduletracer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scheduletracer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
